d10625b172a3e2c50786336ae26af931daedfd91
MitchVac-1 <mvaclaw24@gmail.com>
2026-04-29 15:44:50 -0600

    yeah

---
+++ ponder/dom.js
@@ -0,0 +1,43 @@
+let title = document.querySelector('h1');
+
+
+console.log(title);
+
+title.textContent = 'Web Page Componentsj';
+
+document.querySelector('#topics').style.color = 'red';
+
+const topics = document.querySelector('#topics');
+topics.style.color = 'purple';
+
+const wrapper =document.getElementById('content');
+wrapper.style.backgroundColor = 'lightblue';
+
+let list = document.querySelector('.list');
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+para.style.fontSize = '2em';
+
+
+para.classList.add('background');
+
+const image = document.querySelector('img');
+
+image.setAttribute('src',images/new_logo.png )
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
+
+
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    html.style.color = 'purple';
+    css.style.color = 'purple';
+    js.style.color = 'purple';
+    let codeValue = selectElem.value;
+    document.getElementById(codeValue).style.color = 'red';
+});
+                 
